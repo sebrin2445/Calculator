171af0ca6da9cfb5101b4aab7724de8e925b4df6
sebrin2445 <153431526+sebrin2445@users.noreply.github.com>
2024-05-01 20:27:46 -0600

    adding some functionalities like *,-,/

--- calculator.js
+++ calculator.js
@@ -9,8 +9,28 @@ app.get("/",function(req,res){
 app.post("/",function(req,res){
    var num1=Number(req.body.num1);
    var num2=Number(req.body.num2);
-   var result = num1 + num2;
-    res.send("thanks your value is" + result);
+   var sel=req.body.select;
+   if(sel===1){
+    var result=num1 + num2;
+    res.send("thanks your value is " + result);
+
+   } else if(sel ===2){
+    var result = num1 * num2;
+    res.send("thanks your value is " + result);
+
+   } else if(sel === 3){
+    var result = num1 - num2;
+    res.send("thanks your value is " + result);
+
+
+   } else{
+    var result= num1 / num2;
+    res.send("thanks your value is " + result);
+
+   }
+   console.log(sel);
+   console.log(result);
+   console.log(num1,num2);
 })
 app.listen(3000, function(){
     
